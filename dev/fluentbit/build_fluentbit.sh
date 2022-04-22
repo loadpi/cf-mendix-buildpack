a9d72f62c0a967029291a338382d42d5e910f837
Denis Shuvalov <denis.shuvalov@mendix.com>
2022-04-22 16:10:54 +0200

    build docker added

---
+++ dev/fluentbit/build_fluentbit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Start Fluentbit building..."
+docker build -f Dockerfile -t cloudfoundry/build_fluentbit .
+docker run -d --name fluentbit_build_container cloudfoundry/build_fluentbit:latest
+
+echo "Copy result..."
+docker cp fluentbit_build_container:/fluent-bit-1.9.2/build/bin/fluent-bit ./
+
+echo "Removing container..."
+
+docker stop fluentbit_build_container
+docker rm fluentbit_build_container
+
+echo "Archiving bin file..."
+
+tar -czf fluent-bit-bin.tar.gz fluent-bit
+
+echo "Done"
